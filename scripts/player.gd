826fe9843374c5aa1b8b9998f1c5e25700579202
nf12pro <rayofaik@gmail.com>
2026-07-07 13:35:25 +0100

    Acceleration/Friction added

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
-const original_speed = 200
+@export var original_speed: float = 200
+@export var acceleration: float = 1200
+@export var friction: float = 1200
 
-func get_input():
+var snap_trap: bool = true
+
+func get_input(delta):
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_direction * original_speed
-
+	var target_velocity = input_direction * original_speed
+	if input_direction != Vector2.ZERO:
+		velocity = velocity.move_toward(target_velocity, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 @warning_ignore("unused_parameter")
 func _physics_process(delta):
-	get_input()
+	get_input(delta)
 	move_and_slide()
