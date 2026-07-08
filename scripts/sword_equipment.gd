a679636c1430dd7b1c4790b7889f290f3ecff762
nf12pro <rayofaik@gmail.com>
2026-07-08 19:29:48 +0100

    Sword almost finished
    Need to give enemies take_damage function and add knockback

--- scripts/sword_equipment.gd
+++ scripts/sword_equipment.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
+#region Circles
+@onready var collision_polygon: CollisionPolygon2D = $sword_area/sword_hitbox
+
 @export var radius: float = 80.0
 @export var segments: int = 16
+#endregion
 
-@onready var collision_polygon: CollisionPolygon2D = $sword_area/sword_hitbox
+#region Damaging Opponent 
+var enemy = null
+var damage = 20
+#endregion
 
 func _ready() -> void:
 	generate_semi_circle()
 
 func generate_semi_circle() -> void:
 	var points: PackedVector2Array = PackedVector2Array()
-	
 	points.append(Vector2.ZERO)
 	
 	for i in range(segments + 1):
@@ -20,3 +26,8 @@ func generate_semi_circle() -> void:
 		points.append(Vector2(x, y))
 		
 	collision_polygon.polygon = points
+
+func _on_sword_area_body_entered(body):
+	enemy = body
+	if enemy.has_method("take_damage"):
+		enemy.take_damage(damage)
